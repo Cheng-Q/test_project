8901e3c924fec626b29dcf7a459fed2bbc00fecf
CQ <15939101882@163.com>
2020-11-16 21:49:01 +0800

    测试自动部署2

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,13 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/prettier'
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: 0 // 语句可以不需要分号结尾
   }
-}
+};
